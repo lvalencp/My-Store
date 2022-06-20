77e71fcde79487902466b8cf613014bca40a0832
lvalencp <lvcagua1@misena.edu.co>
2022-06-20 16:41:25 -0500

    20062022

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
+const cors = require('cors')
 const routerApi= require('./routes');
 
 const { logErrors, errorHandler, boomerrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const whitelist = ['http://localhost:5500', 'https://myapp.com'];
+//en el curso utiliza el puerto 8080 pero desde el live serve del computador se utiliza el puerto 5500
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin){
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
